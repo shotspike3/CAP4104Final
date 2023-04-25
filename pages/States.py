b1c4c4e381a8825f9b0b211c368d3954b2668e73
shotspike3 <53101884+shotspike3@users.noreply.github.com>
2023-04-25 16:02:01 -0400

    Update States.py

--- pages/States.py
+++ pages/States.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import requests
 import streamlit as st
@@ -20,16 +21,85 @@ st.markdown(
 )
 
 apiKey = '0a54baab3c394337aa46d1ea286cfb24'
+categories = ['technology', 'politics', 'sports', 'business',
+              'entertainment', 'health', 'general']
 
 finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=technology" \
            f"&apiKey={apiKey}"
 r = requests.get(finalURL).json()
 articles = r['articles']
+
 sources = []
 for article in articles:
     print(article['source']['name'])
     sources += [article['source']['name']]
 print(r)
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=politics" \
+           f"&apiKey={apiKey}"
+rp = requests.get(finalURL).json()
+articles = rp['articles']
+
+sourcesp = []
+for article in articles:
+    print(article['source']['name'])
+    sourcesp += [article['source']['name']]
+print(rp)
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=sports" \
+           f"&apiKey={apiKey}"
+rs = requests.get(finalURL).json()
+articles = rs['articles']
+
+sourcess = []
+for article in articles:
+    print(article['source']['name'])
+    sourcess += [article['source']['name']]
+print(rs)
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=business" \
+           f"&apiKey={apiKey}"
+rb = requests.get(finalURL).json()
+articles = rb['articles']
+
+sourcesb = []
+for article in articles:
+    print(article['source']['name'])
+    sourcesb += [article['source']['name']]
+print(rb)
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=entertainment" \
+           f"&apiKey={apiKey}"
+re = requests.get(finalURL).json()
+articles = re['articles']
+
+sourcese = []
+for article in articles:
+    print(article['source']['name'])
+    sourcese += [article['source']['name']]
+print(re)
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=health" \
+           f"&apiKey={apiKey}"
+rh = requests.get(finalURL).json()
+articles = rh['articles']
+
+sourcesh = []
+for article in articles:
+    print(article['source']['name'])
+    sourcesh += [article['source']['name']]
+print(rh)
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=general" \
+           f"&apiKey={apiKey}"
+rg = requests.get(finalURL).json()
+articles = rg['articles']
+
+sourcesg = []
+for article in articles:
+    print(article['source']['name'])
+    sourcesg += [article['source']['name']]
+print(rg)
 
 source_count = {}
 bruh = []
@@ -60,4 +130,46 @@ bar_chart = alt.Chart(df).mark_bar().encode(
 )
 st.altair_chart(bar_chart, use_container_width=True)
 
-df
+arr = []
+rows, cols = len(sources), 7
+for i in range(rows):
+    col = []
+    for j in range(cols):
+        col.append(0)
+    arr.append(col)
+print(arr)
+cow = 0
+for sub in arr:
+    sub[0] = sources[cow]
+    cow += 1
+
+for i in arr:
+    for c in i:
+        print(c, end=" ")
+    print()
+
+print(arr)
+
+@st.cache_data
+def load_data():
+    return pd.DataFrame(
+        {
+            "Technology": sources,
+            "Politics": sourcesp,
+            "Sports": sourcess,
+            "Business": sourcesb,
+            "Entertainment": sourcese,
+            "Health": sourcesh,
+            "General": sourcesg
+        }
+    )
+
+
+# Boolean to resize the dataframe, stored as a session state variable
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
+
+# Display the dataframe and allow the user to stretch the dataframe
+# across the full width of the container, based on the checkbox value
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
