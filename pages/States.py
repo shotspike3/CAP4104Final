7e334869f9bdd0aefe68c651f6b3a4201804e4bc
shotspike3 <53101884+shotspike3@users.noreply.github.com>
2023-04-25 02:06:25 -0400

    Update States.py

--- pages/States.py
+++ pages/States.py
@@ -1,4 +1,7 @@
+import pandas as pd
+import requests
 import streamlit as st
+import altair as alt
 
 st.markdown(
     """
@@ -15,3 +18,46 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
+apiKey = '0a54baab3c394337aa46d1ea286cfb24'
+
+finalURL = f"https://newsapi.org/v2/top-headlines?country=us&category=technology" \
+           f"&apiKey={apiKey}"
+r = requests.get(finalURL).json()
+articles = r['articles']
+sources = []
+for article in articles:
+    print(article['source']['name'])
+    sources += [article['source']['name']]
+print(r)
+
+source_count = {}
+bruh = []
+for word in sources:
+    if word in source_count:
+        source_count[word] += 1
+    else:
+        source_count[word] = 1
+
+
+bruh += source_count.values()
+
+print(source_count)
+print(len(sources))
+print(len(bruh))
+
+"Number of articles per category"
+df = pd.DataFrame({
+"Sources": [sources],
+"Number of Articles":  [bruh],
+"Categories": [sources]
+})
+
+bar_chart = alt.Chart(df).mark_bar().encode(
+    x="Categories:O",
+    y="Number Of articles:Q",
+    color="Sources:N"
+)
+st.altair_chart(bar_chart, use_container_width=True)
+
+df
